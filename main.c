c29ab4c5b70edd43f485308375e8908082600b31
Mubariz Afzal <mubarizafzal@gmail.com>
2019-11-26 07:27:38 -0500

    created hamming function

--- main.c
+++ main.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <limits.h>
 
-void printBinary(unsigned char byte)
+// using macros for binary conversion?
+
+void printBinary (unsigned char byte)
 {
-    int bCount = CHAR_BIT; 
+    int numBits = CHAR_BIT; 
     
-    while(bCount--) { // is false when i = 0, i.e end of byte
+    while(numBits--) { // is false when i = 0, i.e end of byte
     
-        putchar('0' + ((byte >> bCount) & 1)); // different way to loop?
+        putchar('0' + ((byte >> numBits) & 1));
     }
+}
+
+unsigned char hamming_13_8 (unsigned char byte) {
+  // input in form 0000xxxx
+  
+  // binary or hexadecimal literals?
+    
+  
+  return byte;
 }
 
 int main () {
@@ -27,10 +38,16 @@ int main () {
       putchar('\n');
       printBinary(~byte);
       putchar('\n');
-      printBinary(byte^5); // 5 as a literal is 4 bits, so only first 4 bits are affected
+      printBinary(byte^53); // 53 as a literal is the same bit sequence as '5'
       putchar('\n');
     }
   }
+  
+  int num = 2;
+  num>>1;
+  printf("does shift also assign?: %d\n", num);
+  
+  printf("inc a char by a literal: %c\n", '0' + 2); // %c is '2'
 
   printf("a char is %d byte(s) large\n", sizeof(char));
 
