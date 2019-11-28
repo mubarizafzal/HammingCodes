c59ac6f51621cbb257d717ade22b2bb325cc8627
Mubariz Afzal <mubarizafzal@gmail.com>
2019-11-28 16:16:49 -0500

    beginning matrix manipulation

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 
 // using macros for binary conversion?
 
-void printBinary (unsigned char byte)
-{
+void printBinary (unsigned char byte) {
     int numBits = CHAR_BIT; 
     
     while(numBits--) { // is false when numBits = 0, i.e end of byte
@@ -23,6 +22,13 @@ unsigned char hamming_13_8 (unsigned char byte) {
   
   return byte;
 }
+// D12 D11 D10 D9 C8 D7 D6 D5 C4 D3 C2 C1 C0
+// C0 - DED parity bit - covers all
+// C1 - SEC parity bit - covers 1, 3, 5, 7, 9, 11
+// C2 - SEC parity bit - covers 2, 3, 6, 7, 10, 11
+// C4 - SEC parity bit - covers 4, 5, 6, 7, 12
+// C8 - SEC parity bit - covers 8, 9, 10, 11, 12  
+
 
 int main () {
 
